f36bcdecd8a118ce5ac57cdbf8046bbe30dfbb62
dlancer <dmdpost@gmail.com>
2016-04-05 19:32:30 +0500

    more code comments added, optional parameter naming fix

--- cached_httpbl/api.py
+++ cached_httpbl/api.py
@@ -1,7 +1,7 @@
 import socket
 
 from django.core import cache
-from django.core.exceptions import ImproperlyConfigured, ValidationError
+from django.core.exceptions import ImproperlyConfigured
 
 from ipware.utils import is_valid_ipv4
 
@@ -15,12 +15,17 @@ HTTPBL_THREAT_SPAMMER = 4
 
 class CachedHTTPBL(object):
     def __init__(self, **kwargs):
-        """Instantiate the CachedHTTPBL object."""
+        """
+        Instantiate the CachedHTTPBL object.
+
+        :param kwargs: optional parameters
+        :return:
+        """
 
         api_key = kwargs.get('api_key', None)
         api_host = kwargs.get('api_host', None)
         api_timeout = kwargs.get('api_timeout', None)
-        cache_results = kwargs.get('cache_results', None)
+        use_cache = kwargs.get('use_cache', None)
         cache_backend = kwargs.get('cache_backend', None)
         cache_timeout = kwargs.get('cache_timeout', None)
 
@@ -38,7 +43,7 @@ class CachedHTTPBL(object):
         self._last_result = None
         self._api_host = api_host if api_host else settings.CACHED_HTTPBL_API_HOST
         self._api_timeout = api_timeout if api_timeout else settings.CACHED_HTTPBL_API_TIMEOUT
-        self._use_cache = cache_results if cache_results else settings.CACHED_HTTPBL_USE_CACHE
+        self._use_cache = use_cache if use_cache else settings.CACHED_HTTPBL_USE_CACHE
         self._cache_backend = cache_backend if cache_backend else settings.CACHED_HTTPBL_CACHE_BACKEND
         self._cache_timeout = cache_timeout if cache_timeout else settings.CACHED_HTTPBL_CACHE_TIMEOUT
         self._cache_version = 1
@@ -60,7 +65,6 @@ class CachedHTTPBL(object):
         return 'cached_httpbl_{0}_ip:{1}'.format(self._api_key, ip)
 
     def _request_httpbl(self, ip):
-
         query = '.'.join([self._api_key] + ip.split('.')[::-1] + [self._api_host])
 
         try:
@@ -81,10 +85,12 @@ class CachedHTTPBL(object):
 
     def check_ip(self, ip):
         """
+        Check IP trough the httpBL API
 
         :param ip: ipv4 ip address
-        :return: httpBL check results or None if any error is occurred
+        :return: httpBL results or None if any error is occurred
         """
+
         self._last_result = None
 
         if is_valid_ipv4(ip):
@@ -104,10 +110,20 @@ class CachedHTTPBL(object):
                         'type': type
                     }
                     if self._use_cache:
-                        self._cache.set(key, self._last_result, version=self._cache_version)
+                        self._cache.set(key, self._last_result, timeout=self._api_timeout, version=self._cache_version)
         return self._last_result
 
     def is_threat(self, result=None, harmless_age=None, threat_score=None, threat_type=None):
+        """
+        Check if IP is a threat
+
+        :param result: httpBL results; if None, then results from last check_ip() used (optional)
+        :param harmless_age: harmless age for check if httpBL age is older (optional)
+        :param threat_score: threat score for check if httpBL threat is lower (optional)
+        :param threat_type:  threat type, if not equal httpBL score type, then return False (optional)
+        :return: True or False
+        """
+
         harmless_age = harmless_age if harmless_age is not None else settings.CACHED_HTTPBL_HARMLESS_AGE
         threat_score = threat_score if threat_score is not None else settings.CACHED_HTTPBL_THREAT_SCORE
         threat_type = threat_type if threat_type is not None else -1
@@ -124,15 +140,32 @@ class CachedHTTPBL(object):
         return threat
 
     def is_suspicious(self, result=None):
+        """
+        Check if IP is suspicious
+
+        :param result: httpBL results; if None, then results from last check_ip() used (optional)
+        :return: True or False
+        """
+
         result = result if result is not None else self._last_result
         return True if result['type'] > 0 else False
 
     def invalidate_ip(self, ip):
+        """
+        Invalidate httpBL cache for IP address
+
+        :param ip: ipv4 IP address
+        """
+
         if self._use_cache:
             key = self._make_cache_key(ip)
             self._cache.delete(key, version=self._cache_version)
 
     def invalidate_cache(self):
+        """
+        Invalidate httpBL cache
+        """
+
         if self._use_cache:
             self._cache_version += 1
             self._cache.increment('cached_httpbl_{0}_version'.format(self._api_key))
